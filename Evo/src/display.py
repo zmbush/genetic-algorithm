92b0149f8097e44aeaa7b58df4e0992b3f34f68f
Zach Bush <zach@zmbush.com>
2011-10-22 04:36:23 +0000

    more display work

--- Evo/src/display.py
+++ Evo/src/display.py
@@ -1,6 +1,21 @@
 import pygame
 import sys
 import re
+import random
+
+def selectColor(c):
+   if c == 0:
+      return pygame.Color("black")
+   elif c == 1:
+      return pygame.Color("red")
+   elif c == 2:
+      return pygame.Color("orange")
+   elif c == 3:
+      return pygame.Color("yellow")
+   elif c == 4:
+      return pygame.Color("green")
+   else:
+      return pygame.Color("red")
 
 def main():
    dims = sys.stdin.readline().strip().split(" ")
@@ -9,7 +24,29 @@ def main():
    xdim = int(dims[0])
    ydim = int(dims[1])
    
-   print str(xdim) + ", " + str(ydim)
+   size = [xdim*3, ydim*3]
+
+   pygame.init
+
+   screen = pygame.display.set_mode(size)
+
+   pygame.display.set_caption("Charles Darwin")
+
+   clock = pygame.time.Clock() 
+
+   while True:
+      for x in range(xdim):
+         for y in range(ydim):
+            line = sys.stdin.readline()
+            c = random.randint(0, 5)
+            color = selectColor(c)
+            pygame.draw.rect(screen, color, [x*3, y*3, 3, 3])
+      pygame.display.flip()
+      clock.tick(15)
+
+
+   for line in sys.stdin:
+      print line
 
 
 
